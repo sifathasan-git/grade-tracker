5de03bdb5d62929d2d132a46db625568230ba6b6
sifathasan-git <sifathasan63@gmail.com>
2022-03-27 18:03:15 +0600

    initial

---
+++ src/scripts/semesters.js
@@ -0,0 +1,9 @@
+const semesterDiv = document.querySelector("semesters");
+
+document.getElementById("semesters").innerHTML(`<div class="card">
+<div class="semesterName">
+  <h1>Spring 22</h1>
+  <h1>3.90</h1>
+</div>
+<p>Cse482, Cse498, Cse499</p>
+</div>`);
